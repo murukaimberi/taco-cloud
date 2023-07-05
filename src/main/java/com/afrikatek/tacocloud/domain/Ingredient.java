43a7895d83c7cb3f611f89e318209c6973c7912f
Murukai Gumbo-Mberi <gmurukai@hotmail.com>
2023-07-05 17:22:19 +0200

    model values

---
+++ src/main/java/com/afrikatek/tacocloud/domain/Ingredient.java
@@ -0,0 +1,13 @@
+package com.afrikatek.tacocloud.domain;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+	private String id;
+	private String name;
+	private Type type;
+	public enum Type {
+		WRAP,PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+}
---
+++ src/main/java/com/afrikatek/tacocloud/domain/TacoOrder.java
@@ -0,0 +1,18 @@
+package com.afrikatek.tacocloud.domain;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+	private String deliveryName;
+	private String deliveryStreet;
+	private String deliveryCity;
+	private String deliveryState;
+	private String deliveryZip;
+	private String ccNumber;
+	private String ccExpiration;
+	private String ccCVV;
+	private List<Taco> tacos;
+}
