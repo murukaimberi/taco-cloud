f273f562c4a8c4d2e204ebf8ab3221ef805a718e
Murukai Gumbo-Mberi <gmurukai@hotmail.com>
2023-08-05 13:49:34 +0200

    this is on chapter two before coding the template

--- src/main/java/com/afrikatek/tacocloud/domain/Ingredient.java
+++ src/main/java/com/afrikatek/tacocloud/domain/Ingredient.java
@@ -1,8 +1,12 @@
 package com.afrikatek.tacocloud.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
 	private String id;
 	private String name;
